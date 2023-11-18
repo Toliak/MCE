81e329dd10efeb2160c4c942622494d87b21eb6f
toliak <toliakpurple@gmail.com>
2023-11-18 17:50:12 +0300

    realpath not found attempt to fix

--- mac/prerequirements.sh
+++ mac/prerequirements.sh
@@ -4,8 +4,19 @@ set -eu
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_ROOT_DIR="$SCRIPT_DIR/.."
+PROJECT_DIR="$PROJECT_ROOT_DIR/mac"
+PROJECT_LINUX_DIR="$PROJECT_ROOT_DIR/linux"
 
-source "$PROJECT_LINUX_DIR/_util.sh"
+function checkCommand() {
+  local COMMAND="$1"
+  if ! command -v "$COMMAND" 1>/dev/null 2>/dev/null; then
+    printf "checkCommand: Command \"\e[34m%s\e[0m\" \e[31mnot found\e[0m\n" \
+      "$COMMAND" >&2
+    return 1
+  fi
+
+  return 0
+}
 
 # TODO: colors
 function macPrerequirements() {
@@ -21,6 +32,8 @@ function macPrerequirements() {
     else
         printf "Coreutils found. ok\n"
     fi
+
+    source "$PROJECT_LINUX_DIR/_util.sh"
 
     PROJECT_ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
     PROJECT_DIR=$(realpath "$PROJECT_ROOT_DIR/mac")
